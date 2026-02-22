f28e6bf533f496a471a8d202895206b8d76e629d
bw <auypopo@gmail.com>
2026-02-22 22:01:12 +0800

    init project

---
+++ src/main/java/com/fion/courseenrollapi/CourseEnrollApiApplication.java
@@ -0,0 +1,13 @@
+package com.fion.courseenrollapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseEnrollApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fion/courseenrollapi/CourseEnrollApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fion.courseenrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("success");
+    }
+
+}
